0eaa0aa8a93889074a0dc11cd1b3ba5aa273183e
tim.song <tim.song@avenir.hk>
2025-06-16 16:47:02 +0800

    丰富kafka hook

--- kafka_formatter.go
+++ kafka_formatter.go
@@ -34,12 +34,22 @@ func (f *DefaultKafkaLogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	tag := LogTagUndef
+	if entry.Data != nil {
+		if v, ok := entry.Data[LogTag]; ok {
+			if t, ok := v.(string); ok {
+				tag = t
+			}
+		}
+	}
 	m := map[string]interface{}{
-		"level":    strings.ToUpper(entry.Level.String()),
-		"app":      f.App,
-		"app_name": f.AppName,
-		"env_name": f.EnvName,
-		"message":  string(message),
+		"level":      strings.ToUpper(entry.Level.String()),
+		"app":        f.App,
+		"app_name":   f.AppName,
+		"env_name":   f.EnvName,
+		"log_tag":    tag,
+		"@timestamp": entry.Time.Format(DefaultTimestampFormat),
+		"message":    string(message),
 	}
 	if defaultf, ok := f.Formatter.(*DefaultLogFormatter); ok {
 		m["trace_id"] = defaultf.TraceId
